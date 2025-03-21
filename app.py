eba52502755edfc93175966d8657e4209631d039
JavohirDITE <javohir123321123@gmail.com>
2025-03-21 12:51:46 +0500

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import paramiko
 import os
+import io
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 VPS_HOST = "34.88.223.194"
 VPS_PORT = 22
 VPS_USER = "zokirjonovjavohir61"
-PRIVATE_KEY_PATH = "id_rsa"
+PRIVATE_KEY = os.getenv("SSH_PRIVATE_KEY")  # Берем ключ из переменной окружения
 
 @app.route("/ssh", methods=["POST"])
 def ssh_command():
@@ -17,16 +18,22 @@ def ssh_command():
         return jsonify({"error": "No command provided"}), 400
     
     try:
-        key = paramiko.RSAKey(filename=PRIVATE_KEY_PATH)
+        # Читаем ключ из строки
+        key_file = io.StringIO(PRIVATE_KEY)
+        key = paramiko.RSAKey.from_private_key(key_file)
+
+        # Подключаемся к серверу
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(VPS_HOST, port=VPS_PORT, username=VPS_USER, pkey=key)
         
+        # Выполняем команду
         stdin, stdout, stderr = ssh.exec_command(command)
         output = stdout.read().decode()
+        error = stderr.read().decode()
         ssh.close()
         
-        return jsonify({"output": output})
+        return jsonify({"output": output, "error": error})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
